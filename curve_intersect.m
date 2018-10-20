1858072ca3802e45ccb288e1ac4136fc7bdca41f
Nueva Student <tylerpoon@gmail.com>
2018-10-19 17:33:04 -0700

    curve intersect change

--- curve_intersect.m
+++ curve_intersect.m
@@ -1,6 +1,7 @@
-fol = (x^3+y^3-(3*x*y)) == 0;
-line = (y == x);
-hold on;
-fimplicit(fol);
-fimplicit(line);
-new = find(fol == line);
+syms t
+f = [((3*t)/(1+t^3)); ((3*t^2)/(1+t^3))];
+d = [0 0; 1 2];
+d = paraConv(d);
+
+intersect = solve(f(1) == d(1), f(2) == d(2));
+disp(intersect);
